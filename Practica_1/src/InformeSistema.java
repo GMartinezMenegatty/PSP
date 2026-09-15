a598d91a6e4925d4861822afdfa6a2d7d85c9f9c
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-09-15 10:20:26 +0200

    Practica PSP

---
+++ Practica_1/src/InformeSistema.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public  class InformeSistema {
+
+    public static void main(String[] args){
+        System.out.println("PROCESADORES");
+        System.out.println("--------------------");
+
+        int procesadores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Disponibles JVM : "+ procesadores);
+
+        System.out.println("\nMEMORIA · ANTES");
+        System.out.println("--------------------");
+
+        long memoria_total = Runtime.getRuntime().totalMemory();
+        System.out.println("Total reservada : "+ memoria_total);
+    }
+}
+
+
+
+
